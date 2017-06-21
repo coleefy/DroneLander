ce2da746fb959bf117c4d0b31080ce5b9c083f51
Christian Nícolas Zabala <coleefy@outlook.com>
2017-06-21 15:05:40 +0800

    Add project files.

---
+++ DroneLander/DroneLander/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+
+namespace DroneLander
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new DroneLander.MainPage();
+            MainPage = new NavigationPage(new DroneLander.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start("uwp=45c913fb-f744-4b9c-b783-fb83be59236e;",
+                   typeof(Analytics));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
